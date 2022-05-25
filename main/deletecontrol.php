3c233f7fc3f52cfc1ecccac9d6e194751d1a62a3
Divyam192001 <divyamsaxena2001@gmail.com>
2022-05-25 11:33:38 +0530

    del updated

--- main/deletecontrol.php
+++ main/deletecontrol.php
@@ -1,11 +1,7 @@
 <?php
 	include('../connect.php');
 	$id=$_GET['id'];
-<<<<<<< HEAD
 	$deletquery ="DELETE FROM control WHERE id=$id";
-=======
-	$deletquery ="DELETE FROM user WHERE id=$id";
->>>>>>> a161f453a215175ed000ba9247ddc11987d12941
 	$query= mysqli_query($con,$deletquery);
 	// $result->execute();
 	
